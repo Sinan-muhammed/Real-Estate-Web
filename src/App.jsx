f89502d0c859687e1ed30a3c264f845ad15b640b
Sinan-muhammed <sinasinan418@gmail.com>
2024-05-24 20:26:21 +0530

    Responsive Navbar Compleated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './Layout.scss'
+import Navbar from './Components/NavBar/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <div className='layout'>
+      <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default App
